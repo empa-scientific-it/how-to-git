83445e9bcf8004206d5db6e1eb7ca461478ce6e5
Edoardo Baldi <edoardo.baldi@empa.ch>
2025-07-23 15:58:53 +0200

    Setup tutorial webpage (#16)
    * Setup website structure
    
    * Setup Netlify deployment
    
    * Remove package-lock.json
    
    * Add CustomCard component
    
    * Update version

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeFlexoki from 'starlight-theme-flexoki';
+
+
+export default defineConfig({
+  site: 'https://how-to-git.netlify.app/',
+  integrations: [
+    starlight({
+      title: 'How To Git',
+      description: 'A guide to Git',
+      plugins: [starlightThemeFlexoki()],
+    })
+  ]
+});
