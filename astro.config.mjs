c1cf7fd0bc3a91f1b8cddd225cced143d303e62e
Edoardo Baldi <edoardob90@gmail.com>
2025-07-23 16:50:11 +0200

    Add remark-gemoji plugin
    Render shortcode emojis (e.g. :pencil2:) from Markdown files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeFlexoki from 'starlight-theme-flexoki';
+import remarkGemoji from 'remark-gemoji';
 
 
 export default defineConfig({
   site: 'https://how-to-git.netlify.app/',
+  markdown: {
+    remarkPlugins: [remarkGemoji],
+  },
   integrations: [
     starlight({
       title: 'How To Git',
