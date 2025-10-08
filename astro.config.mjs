940950ba9aafc4ba221dba845e8251627d71e8f4
Edoardo Baldi <edoardo.baldi@empa.ch>
2025-10-08 10:16:16 +0200

    Fix markdown plugins (#35)
    * Fix rehype/remark plugins config
    
    * Add space before emoji
    
    ---------
    
    Co-authored-by: Despina Adamopoulou <despoina.adamopoulou@empa.ch>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,20 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightThemeFlexoki from 'starlight-theme-flexoki';
-import remarkGemoji from 'remark-gemoji';
-import rehypeTargetBlank from './src/plugins/rehype-target-blank.js';
-
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeFlexoki from "starlight-theme-flexoki";
+import remarkGemoji from "remark-gemoji";
+import rehypeTargetBlank from "./src/plugins/rehype-target-blank.js";
 
 export default defineConfig({
-  site: 'https://how-to-git.netlify.app/',
+  site: "https://how-to-git.netlify.app/",
   markdown: {
     remarkPlugins: [remarkGemoji],
+    rehypePlugins: [rehypeTargetBlank],
   },
   integrations: [
     starlight({
-      title: 'How To Git',
-      description: 'A guide to Git',
+      title: "How To Git",
+      description: "A guide to Git",
       plugins: [starlightThemeFlexoki()],
-    })
+    }),
   ],
-  markdown: {
-    rehypePlugins: [rehypeTargetBlank],
-  },
 });
